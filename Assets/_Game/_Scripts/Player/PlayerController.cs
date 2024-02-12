cf3061ed01e3d1268c6a058d4e1db73a6230c026
jamalhaider983 <46599575+jamalhaider983@users.noreply.github.com>
2024-02-13 03:11:43 +0500

    Initial Project Upload

---
+++ Assets/_Game/_Scripts/Player/PlayerController.cs
@@ -0,0 +1,25 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class PlayerController : NetworkBehaviour {
+    [SerializeField] private float _speed = 3;
+    private Rigidbody _rb;
+
+    private void Awake() {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update() {
+        var dir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        _rb.velocity = dir * _speed;
+
+        if(Input.touchCount>0)
+        {
+            transform.position = Input.GetTouch(0).position;
+        }
+    }
+
+    public override void OnNetworkSpawn() {
+        if (!IsOwner) Destroy(this);
+    }
+}
